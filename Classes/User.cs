27691f76ac07a8a6284bd86d4cf83bdee83e034a
wkwestfa <wkwestfa@svsu.edu>
2018-02-04 21:55:00 -0500

    Working on Getting user information
    Added a switch case that works on dealing with commands and getting user information

---
+++ Classes/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchBot
+{
+    enum UserStatus {None, Follower, Subscriber}
+
+    class User
+    {
+        public string strName;
+        public int intPoints;
+
+        public bool blnIsModerator;
+        public bool blnIsSubcribed;
+
+        public User(string name, bool isModerator, bool isSubscribed, int points)
+        {
+            strName = name;
+            intPoints = 0;
+            blnIsModerator = isModerator;
+            blnIsSubcribed = isSubscribed;
+        }
+    }
+}
--- TwitchChatBot.cs
+++ TwitchChatBot.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using TwitchLib;
 using TwitchLib.Models.Client;
 using TwitchLib.Events.Client;
+using TwitchLib.Services;
 using TwitchLib.Models.API.v5.Users;
 using System.Runtime.InteropServices;
 
@@ -9,36 +12,52 @@ namespace TwitchBot
 {
     internal class TwitchChatBot
     {
-        readonly ConnectionCredentials credentials = new ConnectionCredentials(TwitchInfo.BotUsername, TwitchInfo.BotToken);
+        public List<User> lstUsers = new List<User>();
+        
+        readonly ConnectionCredentials credentials = new ConnectionCredentials(Resources.TwitchInfo.BotUsername, Resources.TwitchInfo.BotToken);
         TwitchClient client;
-
+        UserFollow follower;
         internal void Connect()
-        {
-
+        {       
             Console.WriteLine("Connecting");
-
-            client = new TwitchClient(credentials, TwitchInfo.ChannelName, logging: false);
+            client = new TwitchClient(credentials, Resources.TwitchInfo.ChannelName, logging: false);
 
             client.OnLog += Client_OnLog;
             client.OnConnectionError += Client_OnConnectionError;
             client.OnMessageReceived += Client_OnMessageReceived;
+            client.OnUserJoined += Client_OnUserJoined;
             client.Connect();
+        }
+
+        private void Client_OnUserJoined(object sender, OnUserJoinedArgs e)
+        {
+            string strUsername = "";
+            e.Username = strUsername;
+
         }
 
         private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
         {
-            bool firstMessage = false;
+            User newUser;
+            string strMessage = "";
+            strMessage = e.ChatMessage.Message;
 
-            if (e.ChatMessage.Message == "!lastman" && firstMessage == false) {
-                client.SendMessage($" {e.ChatMessage.DisplayName} Wants to start Last Man Standing.  Three participants needed to begin.  To join type '!lastman'");
-                firstMessage = true;
-            }
-            else if (e.ChatMessage.Message == "!lastman") {
-                client.SendMessage($"{e.ChatMessage.DisplayName} Has joined the fray!!");
-            }
-            if(e.ChatMessage.Message.StartsWith("hi", StringComparison.InvariantCultureIgnoreCase))
+            switch(strMessage)
             {
-                client.SendMessage($"Hey there " + e.ChatMessage.DisplayName);
+                case "!lastman":
+                    LastMan(e);      // Execute process for Lastman
+                    break;
+                case "!add":
+                    client.SendMessage("Added");
+                    newUser = new User(e.ChatMessage.Username, e.ChatMessage.IsModerator, e.ChatMessage.IsSubscriber, 0);
+                    using (StreamWriter usersFile = new StreamWriter(@"..\..\Resources\Users.txt"))
+                    {
+                        usersFile.WriteLine($"{newUser.strName} {newUser.blnIsModerator} {newUser.blnIsSubcribed} {newUser.intPoints}");
+                    }
+                    break;
+                default:
+                    break;
+
             }
         }
 
@@ -57,6 +76,25 @@ namespace TwitchBot
             Console.WriteLine("Disconnecting");
         }
 
+
+        private void LastMan(OnMessageReceivedArgs e)
+        {
+            bool firstMessage = false;
+
+            if (firstMessage == false)
+            {
+                client.SendMessage($"{e.ChatMessage.DisplayName} Wants to start Last Man Standing.  Three participants needed to begin.  To join type '!lastman'");
+                firstMessage = true;
+            }
+            else if (e.ChatMessage.Message == "!lastman")
+            {
+                client.SendMessage($"{e.ChatMessage.DisplayName} Has joined the fray!!");
+            }
+            if (e.ChatMessage.Message.StartsWith("hi", StringComparison.InvariantCultureIgnoreCase))
+            {
+                client.SendMessage($"Hey there " + e.ChatMessage.DisplayName);
+            }
+        }
     }
 
 
